1f31da868da109897551060286fef0667ab2a6f8
John Krueger <107435492+JohnKrueger0@users.noreply.github.com>
2025-10-31 15:15:23 -0500

    Support for T7 and T7_Pro at the same time
    Fixed the serial numbers to the DAQs we have. There are now 2 config menus in the sensor config tab. One for each daq.
    
    Both get polled and data is placed on the main interface.

--- V4 - Full App Setup/app/gui/sensor_config_tab.py
+++ V4 - Full App Setup/app/gui/sensor_config_tab.py
@@ -1,11 +1,13 @@
 import dearpygui.dearpygui as dpg
-from gui import T7_daq_config
+from gui import T7_daq_config, T7_pro_daq_config
 
 
 def build():
     with dpg.tab(label="Sensor Config", tag="sensor_config_tab"):
         T7_daq_config.build()
+        T7_pro_daq_config.build()
 
 
 def load_defaults():
     T7_daq_config.load_defaults()
+    T7_pro_daq_config.load_defaults()
